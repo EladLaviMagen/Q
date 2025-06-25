f815bf3f43e762f4268b0e276d0847f346819b30
Elad Magen <eladmagen10@gmail.com>
2025-06-25 13:39:35 +0300

    MR fixes and code improvements

--- roomchatserver.py
+++ roomchatserver.py
@@ -3,6 +3,7 @@ import select
 import argparse
 from collections import defaultdict
 
+
 ADDRESS = "127.0.0.1"
 MAX_USERS = 10
 MAX_MESSAGE_SIZE = 1024
@@ -10,13 +11,21 @@ NAME = 0
 ROOM = 1
 EXIT = "/exit"
 TRANSFER = "/transfer"
-NOTIFY_ROOM_MESSAGE = "has entered the room"
+NOTIFY_ROOM_MESSAGE_ENTRY = "has entered the room"
+NOTIFY_ROOM_MESSAGE_LEAVING = "transferred to a different room"
+NOTIFY_LEAVE_MESSAGE = "left"
 TIMEOUT = 10
-TRANSFER_ERROR_BAD_USAGE = "ERROR : TRANSFER REQUEST WAS NOT PROPER\nUsage : /transfer <room name>\n"
-TRANSFER_ERROR_ADMIN = "ERROR : ADMIN CANNOT TRANSFER\n"
-TRANSFER_ERROR_UNAUTHORIZED = "Unauthorized entry attempt, only admins in the ADMIN room\n"
+VALID = -1
+BAD_USAGE_CODE = 0
+ADMIN_TRANSFER_CODE = 1
+UNAUTHORIZED_ENTRY_CODE = 2
+TRANSFER_ERROR_MESSAGES = ["ERROR : TRANSFER REQUEST WAS NOT PROPER - Usage : /transfer <room name>\n",
+                           "ERROR : ADMIN CANNOT TRANSFER\n",
+                           "ERROR : UNAUTHORIZED ENTRY ATTEMPT TO ADMIN ROOM, ONLY ADMINS IN ADMIN ROOM\n"]
+UNKNOWN_COMMAND_ERROR = "ERROR : UNKNOWN COMMAND ATTEMPTED\n"
 ADMIN = "ADMIN"
-
+COMMAND = '/'
+COMMAND_NAME = 0
 
 """
 Gets server port from arguments
@@ -39,22 +48,18 @@ def handle_new_user(server_socket):
     rooms[entry_message[ROOM]].append(new_socket)
     sockets_info[new_socket] = [entry_message[NAME], entry_message[ROOM]]
     print(entry_message[NAME], "has entered room", entry_message[ROOM])
-    handle_message(new_socket, NOTIFY_ROOM_MESSAGE)
+    handle_message(new_socket, NOTIFY_ROOM_MESSAGE_ENTRY)
 
 
 """
 Handles user messages
 @Param sender_socket - The socket that message was sent from
 @param client_message - The message sent by the client
+@Param delim - Delimeter for message, defaults to space - ' '
 """
-def handle_message(sender_socket, client_message):
+def handle_message(sender_socket, client_message, delim=" "):
     # In the cases of commands, special message are sent for the other users
-    if client_message == EXIT:
-        client_message = sockets_info[sender_socket][NAME] + " left" + '\n'
-    elif client_message.startswith(TRANSFER):
-        client_message = sockets_info[sender_socket][NAME] + " transferred to a different room" + '\n'
-    else:
-        client_message = sockets_info[sender_socket][NAME] + " : " + client_message + '\n'
+    client_message = sockets_info[sender_socket][NAME] + delim + client_message + '\n'
     # Sending message to all the required users
     send_list = rooms[sockets_info[sender_socket][ROOM]] + rooms[ADMIN]
     if sockets_info[sender_socket][ROOM] == ADMIN:
@@ -64,26 +69,15 @@ def handle_message(sender_socket, client_message):
             client.send(client_message.encode())
 
 
-"""
-Handles user room transfer
-@Param transferred_socket - The socket of the user that is transferring rooms
-@param new_room - The new room name to move to
-"""
-def handle_transfer(transferred_socket, new_room):
-    # Updating rooms and client information
-    rooms[sockets_info[transferred_socket][ROOM]].remove(transferred_socket)
-    sockets_info[transferred_socket][ROOM] = new_room
-    rooms[new_room].append(transferred_socket)
-    # Sending a message to the new room that client has joined
-    handle_message(transferred_socket, NOTIFY_ROOM_MESSAGE)
-
 
 """
 Handles user leaving
 @Param leaving_socket - The socket of the user that is leaving
+@Param exit_request - details for the exit request, as of now unused, but I thought of implementing a goodbye message feature to /exit
 """
-def handle_exit(leaving_socket):
+def handle_exit(leaving_socket, exit_request):
     # Updating room and deleting client information
+    handle_message(leaving_socket, NOTIFY_LEAVE_MESSAGE)
     rooms[sockets_info[leaving_socket][ROOM]].remove(leaving_socket)
     sockets_info.pop(leaving_socket)
     wait_list.remove(leaving_socket)
@@ -91,27 +85,43 @@ def handle_exit(leaving_socket):
 
 
 """
-Checks if what the user is attempting to perform/send is valid
-Also sends back error message to user if he sent invalid message
-@Param sender_socket - the socket of the sender
-@param sent_message - the message he is attempting to send to others
-@return True if the message/action is valid and should be handled, false otherwise
+Validates transfer request
+@Param transferred_socket - The socket of the user that is transferring rooms
+@param transfer_request - Parameters of request
+@Return - Result code for transfer request
 """
-def is_valid(sender_socket, sent_message):
-    if sent_message.startswith(TRANSFER):
-        if sockets_info[sender_socket][ROOM] == ADMIN:
-            sender_socket.send(TRANSFER_ERROR_ADMIN.encode())
-            return False
-        sent_message = sent_message.split(' ')
-        if len(sent_message) != 2:
-            sender_socket.send(TRANSFER_ERROR_BAD_USAGE.encode())
-            return False
-        if sent_message[ROOM] == ADMIN:
-            sender_socket.send(TRANSFER_ERROR_UNAUTHORIZED.encode())
-            return False
-    return True
+def validate_transfer(sender_socket, transfer_request):
+    if sockets_info[sender_socket][ROOM] == ADMIN:
+        return ADMIN_TRANSFER_CODE
+    if len(transfer_request) != 2:
+        return BAD_USAGE_CODE
+    if transfer_request[ROOM] == ADMIN:
+        return UNAUTHORIZED_ENTRY_CODE
+    return VALID
 
 
+"""
+Handles user room transfer
+@Param transferred_socket - The socket of the user that is transferring rooms
+@param transfer_request - Parameters of request
+"""
+def handle_transfer(transferred_socket, transfer_request):
+    # Updating rooms and client information
+    result = validate_transfer(transferred_socket, transfer_request)
+    if result == VALID:
+        handle_message(transferred_socket, NOTIFY_ROOM_MESSAGE_LEAVING)
+        rooms[sockets_info[transferred_socket][ROOM]].remove(transferred_socket)
+        sockets_info[transferred_socket][ROOM] = transfer_request[ROOM]
+        rooms[transfer_request[ROOM]].append(transferred_socket)
+        # Sending a message to the new room that client has joined
+        handle_message(transferred_socket, NOTIFY_ROOM_MESSAGE_ENTRY)
+    else:
+        transferred_socket.send(TRANSFER_ERROR_MESSAGES[result].encode())
+
+
+
+
+COMMAND_HANDLER = {EXIT : handle_exit, TRANSFER : handle_transfer}
 rooms = defaultdict(list)
 sockets_info = {}
 wait_list = []
@@ -128,10 +138,12 @@ if __name__ == "__main__":
                 handle_new_user(server)
             else:
                 msg = sock.recv(MAX_MESSAGE_SIZE).decode()
-                if not is_valid(sock, msg):
-                    continue
-                handle_message(sock, msg)
-                if msg == EXIT:
-                    handle_exit(sock)
-                elif msg.startswith(TRANSFER):
-                    handle_transfer(sock, msg.split(' ')[ROOM])
+                if msg[0] == COMMAND:
+                    command_params = msg.split(' ')
+                    if command_params[0] in COMMAND_HANDLER.keys():
+                        COMMAND_HANDLER[command_params[COMMAND_NAME]](sock, command_params)
+                    else:
+                        sock.send(UNKNOWN_COMMAND_ERROR.encode())
+                else:
+                    handle_message(sock, msg, " : ")
+
